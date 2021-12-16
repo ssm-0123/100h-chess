72a43d04030e47ee1431b288a62d1974d308f0eb
hungrybeagle <cyang@deltalearns.ca>
2021-12-16 14:05:49 -0800

    Update x04_queen.py

--- x04_queen.py
+++ x04_queen.py
@@ -1,5 +1,23 @@
-#!python3
+def queen(square):
+  """
+  input:
+  str square: the coordinate of the square that the queen is currently located in
+  examples: 'f3' or 'g6'
+  
+  return:
+  list of possible squares that the queen can move to:
+  """
+  
+  return None
 
-"""
-The queen combines the moves of both the rook and bishop
-"""
+
+def main():
+  myList = queen('f3')
+  myList.sort()
+  assert myList == ['a3', 'a8', 'b3', 'b7', 'c3', 'c6', 'd1', 'd3', 'd5', 'e2', 'e3', 'e4', 'f1', 'f2', 'f4', 'f5', 'f6', 'f7', 'f8', 'g2', 'g3', 'g4', 'h1', 'h3', 'h5']
+  myList = queen('g7')
+  myList.sort()
+  assert myList == ['a1', 'a7', 'b2', 'b7', 'c3', 'c7', 'd4', 'd7', 'e5', 'e7', 'f6', 'f7', 'f8', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g8', 'h6', 'h7', 'h8']
+
+if __name__ == "__main__":
+  main()
